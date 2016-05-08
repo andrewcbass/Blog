cbeab933a77b77cb10e59b7ca1a4bd48798ad03a
andrewcbass <andrew.c.bass@gmail.com>
2016-05-07 17:37:18 -0700

    broken footer

--- home/homeCtrl.js
+++ home/homeCtrl.js
@@ -7,8 +7,12 @@
     homeCtrl.$inject = ["$scope", "$state"]
 
     function homeCtrl($scope, $state) {
-      console.log("helo")
-      let y = 3
+      //define offset for footer in zenscroll, in px
+      let edgeOffset = 60
+      zenscroll.setup(999, edgeOffset)
+      $scope.scroll = function(navTo) {
+        var scrollTo = document.getElementById(navTo)
+        zenscroll.to(scrollTo)
+      }
     }
-
 })();
